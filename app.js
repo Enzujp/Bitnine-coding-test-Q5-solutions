fb06280d20c9b13e2caacd2100bef2a35f31d365
Enzujp <100221308+Enzujp@users.noreply.github.com>
2023-07-27 09:16:18 +0100

    reworked backend

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ const app = express();
 
 const { render } = require('ejs');
 const mongoose = require('mongoose');
+
+const flash = require("connect-flash");
+
 const path = require("path");
 const templatePath = path.join(__dirname, '../templates');
 const collection = require("./mongodb");
+const { request } = require('http');
+
+//connect to mongodb
+const dbURL = "mongodb+srv://enzu:WTTRpJl5yhlpwjKN@cluster0.0bakznq.mongodb.net/"
+mongoose.connect(dbURL)
+    .then((result) => app.listen(3000), () => {
+        console.log("everything works!")    
+    })// app.listen is moved on to ensure we listen for requests only after connection is made 
+    // remember to whitelist ip addresses on mongodb to allow access
+    .catch((err) => console.log(err));
+// register view engine 
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:false}));
 
+app.use(flash);
 
-app.listen(3000, ()=> {
-    console.log("Server now running on port 3000")
-});
 
 
 app.get('/', (req, res) => {
@@ -31,11 +44,28 @@ app.post('/signup', async(req,res)=> {
 
     await collection.insertMany([data]);
 
-    res.render("index")
+    request.flash('message', 'You have signed up Successfully!');
+
+    res.redirect('login') // takes user to login page after signing up
 })
 
 app.post('/login', async (req, res) => {
-    res.render('login')
+    try {
+        const checkDetails = await collection.findOne({
+            name: req.body.name
+        })
+
+        if (checkDetails.password === req.body.password) {
+            res.render(index);
+        }
+        else {
+            res.send("Incorrect Password");
+        }
+    }
+    catch {
+        res.send("Incorrect Details")
+
+    }
 })
 
 app.get('/login', (req, res)=> {
--- mongodb.js
+++ mongodb.js
@@ -1,13 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/BitnineQ5")
-.then(() => {
-    console.log("Database connected!")
-})
-.catch(() => {
-    console.log("Failed to connect to database")
-})
-
 const LogInSchema = new mongoose.Schema({
     username: {
         type: String,
